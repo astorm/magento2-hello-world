60bf96a599fb8e76a97db40a852738ea05b29106
Alan Storm <astorm@alanstorm.com>
2015-09-16 12:53:55 -0700

    Magento 2 Hello World module

---
+++ app/code/Pulsestorm/HelloWorldMVVM/Block/Main.php
@@ -0,0 +1,11 @@
+<?php
+namespace Pulsestorm\HelloWorldMVVM\Block;
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{    
+    protected function _prepareLayout()
+    {
+    
+    }
+}
